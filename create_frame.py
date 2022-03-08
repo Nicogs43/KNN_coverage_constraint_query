9c585e12ba6ee700643c4bfb301ce52e338fb6b7
nico <nico@guainazzo.it>
2022-03-08 11:47:06 +0100

    file for merging different dataframes with the same columns

---
+++ create_frame.py
@@ -0,0 +1,14 @@
+import pandas as pd
+import glob
+path = r'C:\Users\Nicolò\Desktop\Tesi\res - execution'  # use your path
+all_files = glob.glob(path + "/*.csv")
+
+li = []
+
+for filename in all_files:
+    df = pd.read_csv(filename, index_col=None, header=0)
+    li.append(df)
+
+frame = pd.concat(li, axis=0, ignore_index=True)
+pd.set_option("display.max_rows", None, "display.max_columns", None)
+frame.to_csv(r'C:\Users\Nicolò\Desktop\Tesi\res - execution\All_res_execution_dataframes.csv', index=False)
